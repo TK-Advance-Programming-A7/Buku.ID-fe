e4cf6368938eb11701e77ba7426b28350c3f4e44
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-05-25 17:48:40 +0700

    fix: get user with header

--- app/libs/getUser.ts
+++ app/libs/getUser.ts
@@ -4,7 +4,11 @@ export default async function getUserLogin() {
   const cookieStore = cookies();
 
   if (cookieStore.get("token")) {
-    const response = await fetch("http://localhost:8080/api/user/me")
+    const response = await fetch("http://localhost:8080/api/user/me", {
+      headers: {
+        Authorization: `Bearer ${cookieStore.get("token")}`,
+      },
+    });
     const user = await response.json();
     return user;
   } else {
